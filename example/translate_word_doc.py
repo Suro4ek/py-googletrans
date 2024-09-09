53669dfd541b4761a058189744ffd75aaf72d738
1Suro1 <timu.zaxarov@gmail.com>
2024-09-09 20:18:39 +0900

    edit sah

--- example/translate_word_doc.py
+++ example/translate_word_doc.py
@@ -1,30 +1,5 @@
-from docx import Document
+# from docx import Document
 from googletrans import Translator
-
-
-def translate_doc(filename, destination='zh-CN', mix=True):
-    """
-    translate a word document type of file and save the result as document and keep the exactly same file format.
-        :param filename: word doc file
-        :param destination='zh-CN':
-        :param mix=True: if True, will have original language and target language into the same doc. paragraphs by paragraphs.
-    """
-    def tx(t): return Translator().translate(t, dest=destination).text
-    doc = Document(filename)
-    for p in doc.paragraphs:
-        txd = tx(p.text)
-
-        p.text = p.text + ('\n' + txd if mix else '')
-
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                txd = tx(cell.text)
-                p.text = cell.text + ('\n' + txd if mix else '')
-
-    f = filename.replace('.doc', destination.lower() + '.doc')
-    doc.save(f)
-
-if __name__ == '__main__':
-    filename = 'p1.docx'
-    translate_doc(filename)
+translator = Translator()
+result = translator.translate('Этот метод позволит обновлять конкретную версию схемы потока, создавая новую версию на основе указанной.Важно отметить, что при обновлении сущности также нужно обновлять связанные сущности и создавать новые записи в таблице version_control. Это должно происходить в рамках одной транзакции для обеспечения целостности данных', dest='sah', src='ru')
+print(result.text)
